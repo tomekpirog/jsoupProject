27b1dbb9b38971ff3bccc37376da517707ea79bc
Tomasz <t.pirog3@gmail.com>
2020-11-25 12:10:38 +0100

    Try Catch to avoid requests crashing when one site is unavailable

--- src/main/java/pl/drit/learning/WebsiteParser.java
+++ src/main/java/pl/drit/learning/WebsiteParser.java
@@ -20,20 +20,23 @@ class WebsiteParser {
         return processWebsitePage(parsableSiteDTO, parsableSiteDTO.getFirstSiteURL() + URLEncoder.encode(title, StandardCharsets.UTF_8));
     }
 
-    private List<SearchResultDTO> processWebsitePage(ParsableSiteDTO parsableSiteDTO, String pageURL) throws IOException, ParseException {
-        Document document = Jsoup.connect(pageURL).get();
-        for (Element product : document.select(parsableSiteDTO.getProductSelector())) {
-            String title = parsableSiteDTO.getTitleFunction().apply(product);
-            String link = parsableSiteDTO.getLinkFunction().apply(product);
-            double price = PriceParser.parse(parsableSiteDTO.getPriceFunction().apply(product));
-            results.add(new SearchResultDTO(title, price, link));
-            System.out.println(title + " ---------- cena: " + price + " zł // LINK: " + link);
-        }
-
-        String nextPageURL = parsableSiteDTO.getNextPageFunction().apply(document);
-        if (!StringUtil.isBlank(nextPageURL)) {
-            System.out.println(nextPageURL);
-            processWebsitePage(parsableSiteDTO, nextPageURL);
+    private List<SearchResultDTO> processWebsitePage(ParsableSiteDTO parsableSiteDTO, String pageURL) {
+        try {
+            Document document = Jsoup.connect(pageURL).timeout(10 * 1000).get();
+            for (Element product : document.select(parsableSiteDTO.getProductSelector())) {
+                String title = parsableSiteDTO.getTitleFunction().apply(product);
+                String link = parsableSiteDTO.getLinkFunction().apply(product);
+                double price = PriceParser.parse(parsableSiteDTO.getPriceFunction().apply(product));
+                results.add(new SearchResultDTO(title, price, link));
+                System.out.println(title + " ---------- cena: " + price + " zł // LINK: " + link);
+            }
+            String nextPageURL = parsableSiteDTO.getNextPageFunction().apply(document);
+            if (!StringUtil.isBlank(nextPageURL)) {
+                System.out.println(nextPageURL);
+                processWebsitePage(parsableSiteDTO, nextPageURL);
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
         }
         return results;
     }
